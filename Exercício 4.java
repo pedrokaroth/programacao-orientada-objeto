d508791e2d2bb8c801040ae9455e21759afc700d
pedrokaroth <56929916+pedrokaroth@users.noreply.github.com>
2020-04-22 15:30:08 -0300

    Create Exercício 4.java

---
+++ Exercício 4.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+    int matriz[][] = new int[3][3];
+    
+    for(int i = 0; i < 3; i++){
+      for(int x = 0; x < 3; x++){
+        matriz[i][x] = i * x;
+      }
+    }
+
+    exibeMatriz(matriz);
+  }
+
+  public static void exibeMatriz(int[][] matriz) {
+    for(int i = 0; i < 3; i++){
+      for(int x = 0; x < 3; x++){
+        System.out.println(matriz[i][x]);
+      }
+    }
+  }
+}
